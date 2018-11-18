7ca2eb9e958c581e6c638718852b9845f9e0a68b
RoeeZ <roee.zinoue@comm-it.com>
2018-11-18 18:35:36 +0200

    Commit fixes: a. Fix project files and names

---
+++ Web_server/Web_server/Web_server.ino
@@ -0,0 +1,12 @@
+
+
+// the setup function runs once when you press reset or power the board
+#include "SFS_framer.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
